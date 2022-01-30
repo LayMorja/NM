fe282d38130b3c92d688dcb7c9be14bbe4297e27
LayMorja <evil-corporaty@yandex.ru>
2022-01-30 16:04:14 +0300

    Обновил карточки мастеров

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-// const menuBtn = document.querySelector('.m-menu');
-// const sidebar = document.querySelector('.header__block__menu');
-
-// menuBtn.addEventListener('click', function () {
-//    sidebar.classList.toggle('active')
-// })
-
-var mixer = mixitup(".index-menu__row");
+var mixer = mixitup(".types__row");
 
 document.addEventListener('DOMContentLoaded', () => {
-  const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+   const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
 
-    const header = document.querySelector('.header') // находим header и записываем в константу
+   const header = document.querySelector('.header') // находим header и записываем в константу
 
-    let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
-    let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+   let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
+   let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
 
-    window.addEventListener('scroll', () => { // при прокрутке страницы
+   window.addEventListener('scroll', () => { // при прокрутке страницы
 
       currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
 
@@ -30,103 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
 
-    })
-  }
-  onScrollHeader()
-});
-
-
-const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll('.lock-padding');
-
-let unlock = true;
-
-const timeout = 400;
-
-if (popupLinks.length > 0) {
-   for (let index = 0; index < popupLinks.length; index++) {
-      const popupLink = popupLinks[index];
-      popupLink.addEventListener('click', function(e) {
-         const popupName = popupLink.getAttribute('href').replace('#', '');
-         const curentPopup = document.getElementById(popupName);
-         popupOpen(curentPopup);
-         e.preventDefault();
-      });
-   }
-}
-const popupCloseIcon = document.querySelectorAll('.close-popup');
-if (popupCloseIcon.length > 0) {
-   for (let index = 0; index < popupCloseIcon.length; index++) {
-      const el = popupCloseIcon[index];
-      el.addEventListener('click', function(e) {
-         popupClose(el.closest('.popup'));
-         e.preventDefault();
       })
    }
-}
-
-function popupOpen(curentPopup) {
-   if (curentPopup && unlock) {
-      const popupActive = document.querySelector('.popup.open');
-      if (popupActive) {
-         popupCloseIcon(popupActive, false);
-      } else {
-         bodyLock();
-      }
-      curentPopup.classList.add('open');
-      curentPopup.addEventListener('click', function(e) {
-         if (!e.target.closest('.popup__content')) {
-            popupClose(e.target.closest('.popup'));
-         }
-      });
-   }
-}
-function popupClose(popupActive, doUnlock = true) {
-   if(unlock) {
-      popupActive.classList.remove('open');
-      if(doUnlock) {
-         bodyUnlock();
-      }
-   }
-}
-
-function bodyLock() {
-   const lockPaddinValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-
-   if (lockPadding.length > 0) {
-      for(let index = 0; index < lockPaddinValue.length; index++) {
-         const el = lockPadding[index];
-         el.style.paddingRight = lockPaddinValue;
-      }
-      body.style.paddingRight = lockPaddinValue;
-      body.classList.add('lock');
-   }
-
-   unlock = false;
-   setTimeout(function() {
-      unlock = true;
-   }, timeout);
-}
-function bodyUnlock() {
-   setTimeout(function() {
-      for(let index = 0; index < lockPadding.length; index++) {
-         const el = lockPadding[index];
-         el.style.paddingRight = '0px';
-      }
-      body.style.paddingRight = '0px';
-      body.classList.remove('lock');
-   }, timeout);
-
-   unlock = false;
-   setTimeout(function() {
-      unlock = true;
-   }, timeout);
-}
-
-document.addEventListener('keydown', function(e) {
-   if(e.which === 27) {
-      const popupActive = document.querySelector('.popup.open');
-      popupClose(popupActive);
-   }
-})
+   onScrollHeader()
+});
